d8b90fbb73a855b9ad8ad4169b82786418f33ff6
jorgejacinto9701 <jorgejacinto9701@gmail.com>
2023-11-15 21:31:08 -0500

    terminado

--- src/app/components/boleta/boleta.component.ts
+++ src/app/components/boleta/boleta.component.ts
@@ -23,6 +23,7 @@ export class BoletaComponent {
 
   objCliente : Cliente = {};
   objProducto : Producto = {};
+
   dataSource:any;
   @ViewChild(MatPaginator, { static: true }) paginator!: MatPaginator;
   displayedColumns = ["idProducto","nombre","precio","cantidad",'actions'];
@@ -40,30 +41,99 @@ export class BoletaComponent {
   ngOnInit(): void {}
 
   buscaCliente(){
-   
+      console.log(">>>> buscaCliente >>> ");
+      const dialogRef = this.dialogService.open(ModelClienteComponent);
+      dialogRef.afterClosed().subscribe( () => this.cargaCliente() );
   }
 
   cargaCliente(){
-    
+      console.log(">>>> cargaCliente >>> ");
+      var str = window.sessionStorage.getItem("CLIENTE")|| '{}';
+      this.objCliente = JSON.parse(str);
+      console.log(">>>> cargaCliente >>> objCliente >>> "  + this.objCliente.nombre );
+      console.log(">>>> cargaCliente >>> objCliente >>> "  + this.objCliente.apellido );
   }
 
   buscaProducto(){
-   
+    console.log(">>>> buscaProducto >>> ");
+    const dialogRef = this.dialogService.open(ModelProductoComponent);
+    dialogRef.afterClosed().subscribe( () => this.cargaProducto() );
   }
 
   cargaProducto(){
-    
+    console.log(">>>> cargaProducto >>> ");
+    var str = window.sessionStorage.getItem("PRODUCTO")|| '{}';
+    this.objProducto = JSON.parse(str);
+    console.log(">>>> cargaProducto >>> objProducto >>> "  + this.objProducto.nombre );
+    console.log(">>>> cargaProducto >>> objProducto >>> "  + this.objProducto.categoria?.descripcion );
   }
 
   agregarProducto(){
-   
+    if (this.objProducto.stock! < this.objProducto.cantidad! ) {
+        Swal.fire({title: "Validación",text: "La cantidad  es mayor al stock" ,icon: "info" });
+        return;
+    }
+
+    let noExiste = true;
+    this.lstProductos.forEach( (item, index) => { 
+          if (item.idProducto == this.objProducto.idProducto ){
+                this.lstProductos[index] = this.objProducto;
+                noExiste = false;              
+          }
+     });
+
+    if (noExiste){
+          this.lstProductos.push(this.objProducto);
+    }
+
+    this.dataSource = new MatTableDataSource<Producto>(this.lstProductos);
+    this.dataSource.paginator = this.paginator;
   }
 
   eliminaProducto(objProducto: Producto){
-   
+    this.lstProductos.forEach( (item, index) => { 
+        if (item.idProducto == objProducto.idProducto ){
+               this.lstProductos.splice(index);
+        }
+    });
+
+    this.dataSource = new MatTableDataSource<Producto>(this.lstProductos);
+    this.dataSource.paginator = this.paginator;
   }
+
   registrarBoleta(){
-     
+     let lstDetalles : BoletaHasProducto [] = [];
+    
+     this.lstProductos.forEach( (item) => {
+          var objPK :  BoletaHasProductoPK = {
+              idProducto : item.idProducto
+          }
+
+          var objDetalle : BoletaHasProducto = {
+                cantidad : item.cantidad,
+                precio : item.precio,
+                producto : item,
+                boletaHasProductoPK : objPK
+          }
+
+          lstDetalles.push(objDetalle);
+     });
+
+     let objBoleta : Boleta = {
+          cliente: this.objCliente,
+          usuario : this.objUsuario,
+          detallesBoleta : lstDetalles
+      };
+
+      this.boletaService.inserta(objBoleta).subscribe( x => {
+            Swal.fire({title: "Mensaje",text: x.mensaje ,icon: "info" });
+            this.objCliente = {};
+            this.objProducto = {};
+            this.lstProductos = [];
+            this.dataSource = new MatTableDataSource<Producto>(this.lstProductos);
+            this.dataSource.paginator = this.paginator;
+      });
+
   }
   
 }
